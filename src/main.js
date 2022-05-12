759cfb823007e179ad227c724bb29c5b9dd34a75
Filipe <filipelehmannp@gmail.com>
2022-05-11 21:48:24 -0300

    demo user function

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import { projectAuth } from './firebase/config'
 
+
 // Global Styles
 import './assets/main.css'
 
--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,12 +1,15 @@
 <template>
-    <form @submit.prevent="handleSubmit">
-        <h3>Login</h3>
-        <input type="email" placeholder="Email" v-model="email" />
-        <input type="password" placeholder="Password" v-model="password" />
-        <div v-if="error" class="error">{{ error }}</div>
-        <button v-if="!isPending">Log in</button>
-        <button v-if="isPending" disabled>Loading</button>
-    </form>
+  <form id="login-form" @submit.prevent="handleSubmit">
+    <h3>Login</h3>
+    <input type="email" placeholder="Email" v-model="email" />
+    <input type="password" placeholder="Password" v-model="password" />
+    <div v-if="error" class="error">{{ error }}</div>
+    <div class="buttons">
+      <button v-if="!isPending" type="submit" form="login-form">Log in</button>
+      <button v-if="!isPending" @click="demoSubmit">Demo</button>
+      <button class="demos" v-if="isPending" disabled>Loading</button>
+    </div>
+  </form>
 </template>
 
 <script>
@@ -16,26 +19,40 @@ import { ref } from "vue";
 import { useRouter } from "vue-router";
 
 export default {
-    setup() {
-        const { error, login, isPending } = useLogin();
-        const email = ref("");
-        const password = ref("");
-        const displayName = ref("");
-        const router = useRouter();
+  setup() {
+    const { error, login, isPending } = useLogin();
+    const email = ref("");
+    const password = ref("");
+    const displayName = ref("");
+    const router = useRouter();
+    const demoUser = ["demo@user.com", "demodemo"];
 
-        const handleSubmit = async () => {
-            const res = await login(
-                email.value,
-                password.value,
-                displayName.value
-            );
-            if (error.value) {
-                console.log(error.value);
-            } else {
-                router.push({ name: "Home" });
-            }
-        };
-        return { email, password, displayName, handleSubmit, error, isPending };
-    },
+// , displayName.value
+    const handleSubmit = async () => {
+      const res = await login(email.value, password.value);
+      if (error.value) {
+        console.log(error.value);
+      } else {
+        router.push({ name: "Home" });
+      }
+    };
+    const demoSubmit = async () => {
+      const res = await login(demoUser[0], demoUser[1]);
+      if (error.value) {
+        console.log(error.value);
+      } else {
+        router.push({ name: "Home" });
+      }
+    };
+    return {
+      email,
+      password,
+      displayName,
+      handleSubmit,
+      demoSubmit,
+      error,
+      isPending,
+    };
+  },
 };
 </script>
