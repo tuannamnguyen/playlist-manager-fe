0890ff3ff12d534a04c6dd1724ad6e6cf76424ab
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-08-21 23:18:39 +0700

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <Navbar />
   <div class="content">
-    <RouterView></RouterView>
+    <router-view></router-view>
   </div>
 </template>
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,4 +1,60 @@
 <template>
-    <p>Navbar hello world</p>
-
+    <div class="navbar">
+        <nav>
+            <img src="@/assets/token.png">
+            <h1>
+                <router-link to="/">Playlist manager</router-link>
+            </h1>
+            <div class="links">
+                <div>
+                    <router-link to="/create_playlist">Create Playlist</router-link>
+                    <router-link to="/my_playlist">My Playlist</router-link>
+                </div>
+                <span>Hi there, Nam</span>
+            </div>
+            <button>Sign Up</button>
+            <button>Log In</button>
+        </nav>
+    </div>
 </template>
+
+<style scoped>
+.navbar {
+    padding: 16px 10px;
+    margin-bottom: 60px;
+    background: white;
+}
+
+nav {
+    display: flex;
+    align-items: center;
+    max-width: 1200px;
+    margin: 0 auto;
+}
+
+nav img {
+    max-height: 60px;
+}
+
+nav h1 {
+    margin-left: 20px;
+}
+
+nav .links {
+    margin-left: auto;
+}
+
+nav .links a,
+button {
+    margin-top: 16px;
+    font-size: 14px;
+}
+
+span {
+    font-size: 14px;
+    display: inline-block;
+    margin-left: 16px;
+    padding-left: 16px;
+    border-left: 1px solid #eee;
+}
+</style>
