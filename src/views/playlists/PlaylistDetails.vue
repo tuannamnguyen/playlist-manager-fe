55f4b8099c9cb1d1be7292bc23cf257891b93966
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-09-15 22:24:06 +0700

    delete playlist

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -3,12 +3,14 @@
     <div v-if="playlist" class="playlist-details">
         <div class="playlist-info">
             <div class="cover">
-                <img :src="'https://picsum.photos/200/300'" :alt="songs.playlist_name" />
+                <img :src="'https://picsum.photos/200/300'" :alt="playlist.playlist_name" />
             </div>
             <h2>{{ playlist.playlist_name }}</h2>
             <p>Created by {{ playlist.user_name }}</p>
             <p class="description">Lorem ipsum</p>
-            <button v-if="ownership">Delete Playlist</button>
+            <button v-if="ownership" @click="handleDeletePlaylist" :disabled="isDeleting">
+                {{ isDeleting ? 'Deleting...' : 'Delete Playlist' }}
+            </button>
         </div>
 
         <div class="song-list">
@@ -18,22 +20,21 @@
             <div v-for="(song, index) in songs" :key="index" class="single-song">
                 <div class="details">
                     <h3>{{ song.song_name }}</h3>
-                    <p>{{ song.artist_names }}</p>
-                    <p v-for="artist in songs.artist_names">{{ artist }}, </p>
+                    <p>{{ song.artist_names.join(', ') }}</p>
                 </div>
-                <button  v-if="ownership">delete</button>
+                <button v-if="ownership">delete</button>
             </div>
         </div>
     </div>
 </template>
 
-
 <script>
 import getSongsInPlaylist from '@/composables/getSongsInPlaylist';
 import getPlaylist from '@/composables/getPlaylist';
+import deletePlaylist from '@/composables/deletePlaylist';
 import { useAuth0 } from '@auth0/auth0-vue';
 import { computed, onMounted, ref } from 'vue';
-
+import { useRouter } from 'vue-router';
 
 export default {
     props: ["id"],
@@ -42,14 +43,15 @@ export default {
         const songs = ref([]);
         const playlist = ref({});
         const isPending = ref(true);
+        const isDeleting = ref(false);
         const { user } = useAuth0();
+        const router = useRouter();
 
         onMounted(async () => {
             try {
                 const playlistResult = await getPlaylist(props.id);
                 playlist.value = playlistResult.playlist.value || {};
                 error.value = playlistResult.error.value;
-
 
                 const result = await getSongsInPlaylist(props.id);
                 songs.value = result.songs.value || [];
@@ -69,7 +71,31 @@ export default {
                 user.value.sub == playlist.value.user_id
             );
         })
-        return { error, isPending, songs, playlist, ownership };
+
+        const handleDeletePlaylist = async () => {
+            if (!confirm('Are you sure you want to delete this playlist?')) {
+                return;
+            }
+
+            isDeleting.value = true;
+            error.value = null;
+
+            try {
+                const { error: deleteError } = await deletePlaylist(props.id);
+                if (deleteError.value) {
+                    throw new Error(deleteError.value);
+                }
+                // Redirect to home page after successful deletion
+                router.push({ path: '/', query: { deletedPlaylist: playlist.value.playlist_name } });
+            } catch (e) {
+                error.value = e.message || "Failed to delete playlist";
+                console.error('Error deleting playlist:', e);
+            } finally {
+                isDeleting.value = false;
+            }
+        };
+
+        return { error, isPending, songs, playlist, ownership, handleDeletePlaylist, isDeleting };
     }
 }
 </script>
